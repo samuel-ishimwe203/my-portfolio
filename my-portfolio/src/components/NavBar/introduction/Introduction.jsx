e57338af63bd03c95a07a6b66b97692eb7615e37
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-12-29 23:29:45 +0200

    chore: navigation button css is added

--- my-portfolio/src/components/NavBar/introduction/Introduction.jsx
+++ my-portfolio/src/components/NavBar/introduction/Introduction.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './Introduction'
+import {Link} from 'react-scroll'
+import cas from '../../../assets/case.png'
 
 function Introduction() {
-  return (
+  return ( 
      <section id="introduction">
-       
+        <div className="introContent">
+          <span className="hello"></span>
+          <span className="introText">I'm <span className="introname "></span> <br />Web application developer  </span>
+          <p className="introprag">I am a skilled in software development with experience in creating visually appealing and user friendly websites </p>
+         <Link><button className="btn><img src={cas} alt="Hire me " />Hire me </button></Link>
+        </div>
+         
+       <img src=" " alt=""    className=''/>
      </section>
   )
 }
