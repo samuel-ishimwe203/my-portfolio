81d6ff9b59fde333642d82b6be7056cd745403cf
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-12-29 22:16:26 +0200

    chore: navigations css to menu added in navBar

--- my-portfolio/src/components/NavBar/Navbar.jsx
+++ my-portfolio/src/components/NavBar/Navbar.jsx
@@ -7,20 +7,19 @@ function Navbar() {
   return (
         <nav className="navBar">
 
-            <img src={logo} alt="logo" className='logo' />
+           <img src={logo} alt="Samuel Ishimwe Logo" className='logo' />
+
 
             <div className="navigations">
 
                 <Link className="HomeMenu" >Home</Link>
-                <Link className="HomeMenu" >About</Link>
-                <Link className="HomeMenu" >portfolio</Link>
-                <Link className="HomeMenu" >clients</Link>
+                <Link className="aboutMenu" >About</Link>
+                <Link className="folioMenu" >portfolio</Link>
+                <Link className="clientsMenu" >clients</Link>
 
             </div>
 
-            <button className="btnMenu"> contact me
-
-            </button>
+            <button className="btnMenu"> contact me</button>
 
         </nav>
   )
