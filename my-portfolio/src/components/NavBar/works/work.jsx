adcb08e0faf30644c2e87fd8c6505d49dbff242f
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-12-31 13:37:46 +0200

    chore: work component is added

--- my-portfolio/src/components/NavBar/works/work.jsx
+++ my-portfolio/src/components/NavBar/works/work.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import './work.css'
+import img1 from '../../../assets/image1.png'
+import img2 from '../../../assets/image2.png'
+import img3 from '../../../assets/image3.png'
+import img4 from '../../../assets/image4.png'
+import img5 from '../../../assets/image5.png'
+import img6 from '../../../assets/web.png'
+
 
 function work() {
   return (
-    <div>work</div>
+  <section id="works">
+
+      <h2 className="worksTitle">My portfolio</h2>
+      <span className="workDesc">I focus on building clean, modern, and functional web applications with attention to detail and performance. My work combines good design, user experience, and solid development practices to create reliable digital solutions for real-world problems.</span>
+      <div className="workImgs">
+         <img src={img1} alt="" className="worksimg" />
+          <img src={img2} alt="" className="worksimg" />
+           <img src={img3} alt="" className="worksimg" />
+            <img src={img4} alt="" className="worksimg" />
+             <img src={img5 } alt="" className="worksimg" />
+              <img src={img6} alt="" className="worksimg" />
+      </div>
+  </section>
   )
 }
 
